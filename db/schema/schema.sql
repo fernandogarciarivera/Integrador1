7e5c2a0dc86d21f75cbff2b60eb7fadfbe24c49a
Fernando <fernandogarciarivera@gmail.com>
2026-09-18 20:51:51 +0000

    Update schema from drawDB
    Co-authored-by: drawDB <no-reply@drawdb.app>

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -131,18 +131,55 @@ CREATE TABLE IF NOT EXISTS `formularioApp` (
 
 
 CREATE TABLE IF NOT EXISTS `formularioAcceso` (
-	`id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-	`idFormularioApp` INTEGER,
+	`idFormularioAcceso` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 	`idPerfilAcceso` INTEGER,
-	PRIMARY KEY(`id`)
+	`idFormularioApp` INTEGER,
+	PRIMARY KEY(`idFormularioAcceso`)
 );
 
 
 CREATE TABLE IF NOT EXISTS `colaborador` (
 	`idColaborador` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-	`idEmpresa` INTEGER NOT NULL,
+	`idEmpresa` INTEGER,
 	`idLocalEmpresa` INTEGER NOT NULL,
 	PRIMARY KEY(`idColaborador`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `estadoAviso` (
+	`idEstadoAviso` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	`textoEstado` VARCHAR(20),
+	PRIMARY KEY(`idEstadoAviso`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `estadoAvisoFormulario` (
+	`idAvisoFormulario` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	`idEstadoAviso` INTEGER,
+	`idFormularioAcceso` INTEGER,
+	PRIMARY KEY(`idAvisoFormulario`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `pedidoLocal` (
+	`idPedidoLocal` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	`idLocalEmpresa` INTEGER,
+	`idEmpresa` INTEGER,
+	`pedidoLocal` VARCHAR(50),
+	`QRLocal` TEXT(1024),
+	PRIMARY KEY(`idPedidoLocal`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `metricaPedido` (
+	`idMetricaPedido` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+	`idPedidoLocal` INTEGER,
+	`idFormularioApp` INTEGER,
+	`idEstadoAviso` INTEGER,
+	`fechaMetrica` DATE,
+	`HoraInicio` TIMESTAMP,
+	`HoraFin` TIMESTAMP,
+	PRIMARY KEY(`idMetricaPedido`)
 );
 
 
@@ -164,12 +201,27 @@ ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `perfilAcceso`
 ADD FOREIGN KEY(`idPerfilAcceso`) REFERENCES `formularioAcceso`(`idPerfilAcceso`)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE `empresaContrata`
-ADD FOREIGN KEY(`idEmpresa`) REFERENCES `colaborador`(`idEmpresa`)
-ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `localEmpresa`
-ADD FOREIGN KEY(`idLocalEmpresa`) REFERENCES `colaborador`(`idLocalEmpresa`)
+ADD FOREIGN KEY(`idLocalEmpresa`, `idEmpresa`) REFERENCES `colaborador`(`idLocalEmpresa`, `idEmpresa`)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE `colaborador`
 ADD FOREIGN KEY(`idColaborador`) REFERENCES `users`(`idColaborador`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `estadoAviso`
+ADD FOREIGN KEY(`idEstadoAviso`) REFERENCES `estadoAvisoFormulario`(`idEstadoAviso`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `formularioApp`
+ADD FOREIGN KEY(`idFormularioApp`) REFERENCES `estadoAvisoFormulario`(`idFormularioAcceso`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `localEmpresa`
+ADD FOREIGN KEY(`idLocalEmpresa`, `idEmpresa`) REFERENCES `pedidoLocal`(`idLocalEmpresa`, `idEmpresa`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `pedidoLocal`
+ADD FOREIGN KEY(`idPedidoLocal`) REFERENCES `metricaPedido`(`idPedidoLocal`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `estadoAviso`
+ADD FOREIGN KEY(`idEstadoAviso`) REFERENCES `metricaPedido`(`idEstadoAviso`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `formularioApp`
+ADD FOREIGN KEY(`idFormularioApp`) REFERENCES `metricaPedido`(`idEstadoAviso`)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
